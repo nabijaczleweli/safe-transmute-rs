b6f4f570e06037a05f638aa4e5853e9a72a06fd8
Eduardo Pinho <enet4mikeenet@gmail.com>
2018-03-05 15:29:57 +0000

    Add a few tests containing unaligned slicing

--- tests/guarded_transmute_many/mod.rs
+++ tests/guarded_transmute_many/mod.rs
@@ -41,3 +41,27 @@ fn too_much() {
                    Ok([0x0100u16, 0x0200u16, 0x0300u16].into_iter().as_slice()));
     }
 }
+
+#[cfg(target_endian = "little")]
+#[test]
+fn misaligned_slicing() {
+    let bytes = &[0xFF, 0x01, 0xEE, 0x02, 0xDD, 0x03, 0xCC];
+    unsafe {
+        assert_eq!(guarded_transmute_many::<u16>(bytes),
+                   Ok([0x01FF, 0x02EE, 0x03DD].into_iter().as_slice()));
+        assert_eq!(guarded_transmute_many::<u16>(&bytes[1..]),
+                   Ok([0xEE01, 0xDD02, 0xCC03].into_iter().as_slice()));
+    }
+
+    let bytes: &[u8] = &[0xFF, 0x01, 0xEE, 0x02, 0xDD, 0x03, 0xCC, 0x04, 0xBB, 0x05, 0xAA, 0x06];
+    unsafe {
+        assert_eq!(guarded_transmute_many::<u32>(bytes),
+                   Ok([0x02EE01FF, 0x04CC03DD, 0x06AA05BB].into_iter().as_slice()));
+        assert_eq!(guarded_transmute_many::<u32>(&bytes[1..]),
+                   Ok([0xDD02EE01, 0xBB04CC03].into_iter().as_slice()));
+        assert_eq!(guarded_transmute_many::<u32>(&bytes[2..]),
+                   Ok([0x03DD02EE, 0x05BB04CC].into_iter().as_slice()));
+        assert_eq!(guarded_transmute_many::<u32>(&bytes[3..]),
+                   Ok([0xCC03DD02, 0xAA05BB04].into_iter().as_slice()));
+    }
+}
