nabijaczleweli/safe-transmute-rs

commit f3c892034d1624b99856c2a22c5ffa3b6bdd4e31

Fix tests that get broken by rustc's new byte slice layout

src/full.rs
@@ -46,7 +46,7 @@ use alloc::vec::Vec;
 /// # /*
 /// assert_eq!(transmute_one::<u32>(&[0x00, 0x00, 0x00, 0x01])?, 0x0100_0000);
 /// # */
-/// # assert_eq!(transmute_one::<u32>(&[0x00, 0x00, 0x00, 0x01].le_to_native::<u32>()).unwrap(), 0x0100_0000);
+/// # assert_eq!(transmute_one::<u32>(&Le2NAl4([0x00, 0x00, 0x00, 0x01]).0.le_to_native::<u32>()).unwrap(), 0x0100_0000);
 /// # }
 /// ```
 pub fn transmute_one<T: TriviallyTransmutable>(bytes: &[u8]) -> Result<T, Error<u8, T>> {
@@ -77,7 +77,7 @@ pub fn transmute_one<T: TriviallyTransmutable>(bytes: &[u8]) -> Result<T, Error<
 /// # /*
 /// assert_eq!(transmute_one_pedantic::<u16>(&[0x0F, 0x0E])?, 0x0E0F);
 /// # */
-/// # assert_eq!(transmute_one_pedantic::<u16>(&[0x0F, 0x0E].le_to_native::<u16>()).unwrap(), 0x0E0F);
+/// # assert_eq!(transmute_one_pedantic::<u16>(&Le2NAl2([0x0F, 0x0E]).0.le_to_native::<u16>()).unwrap(), 0x0E0F);
 /// # }
 /// ```
 pub fn transmute_one_pedantic<T: TriviallyTransmutable>(bytes: &[u8]) -> Result<T, Error<u8, T>> {
@@ -106,7 +106,7 @@ pub fn transmute_one_pedantic<T: TriviallyTransmutable>(bytes: &[u8]) -> Result<
 /// # /*
 /// assert_eq!(transmute_many::<u16, SingleManyGuard>(&[0x00, 0x01, 0x00, 0x02])?,
 /// # */
-/// # assert_eq!(transmute_many::<u16, SingleManyGuard>(&[0x00, 0x01, 0x00, 0x02].le_to_native::<u16>()).unwrap(),
+/// # assert_eq!(transmute_many::<u16, SingleManyGuard>(&Le2NAl4([0x00, 0x01, 0x00, 0x02]).0.le_to_native::<u16>()).unwrap(),
 ///            &[0x0100, 0x0200]);
 /// # }
 /// ```
@@ -161,7 +161,7 @@ pub fn transmute_many_permissive<T: TriviallyTransmutable>(bytes: &[u8]) -> Resu
 /// # /*
 /// assert_eq!(transmute_many_pedantic::<u16>(&[0x0F, 0x0E, 0x0A, 0x0B])?,
 /// # */
-/// # assert_eq!(transmute_many_pedantic::<u16>(&[0x0F, 0x0E, 0x0A, 0x0B].le_to_native::<u16>()).unwrap(),
+/// # assert_eq!(transmute_many_pedantic::<u16>(&Le2NAl4([0x0F, 0x0E, 0x0A, 0x0B]).0.le_to_native::<u16>()).unwrap(),
 ///            &[0x0E0F, 0x0B0A]);
 /// # }
 /// ```

src/lib.rs
@@ -127,11 +127,12 @@
 //! # include!("../tests/test_util/le_to_native.rs");
 //! # fn main() {
 //! assert_eq!(transmute_one::<f64>(
-//!     &[0x00, 0x00, 0x00, 0x00,
 //! # /*
+//!     &[0x00, 0x00, 0x00, 0x00,
 //!       0x00, 0x00, 0x00, 0x40])?,
 //! # */
-//! #     0x00, 0x00, 0x00, 0x40].le_to_native::<f64>()).unwrap(),
+//! #   &Le2NAl8([0x00, 0x00, 0x00, 0x00,
+//! #             0x00, 0x00, 0x00, 0x40]).0.le_to_native::<f64>()).unwrap(),
 //!     2.0);
 //! # }
 //! ```

tests/test_util/le_to_native.rs
@@ -1,6 +1,18 @@
 extern crate core as le_to_native_core;
 
 
+/// Test aids: rustc has started placing static byte arrays at odd offsets
+#[repr(align(64))]
+#[allow(dead_code)]
+struct Le2NAl2([u8; 2]);
+#[repr(align(64))]
+#[allow(dead_code)]
+struct Le2NAl4([u8; 4]);
+#[repr(align(64))]
+#[allow(dead_code)]
+struct Le2NAl8([u8; 8]);
+
+
 /// Verify: http://play.integer32.com/?gist=4cd795d6f45898c876a754cd3f3c2aaa&version=stable
 trait LeToNative {
     fn le_to_native<T: Sized>(self) -> Self;

commit 6e9520a6d9acdf5eac96b5dc8a15aa241cd2e0b7

Force-align remaining doctests
Ref: #67
Fixes: f3c892034d1624b99856c2a22c5ffa3b6bdd4e31 ("Fix tests that get
broken by rustc's new byte slice layout")

src/base.rs
@@ -44,7 +44,7 @@ use core::slice;
 /// # /*
 ///     assert_eq!(from_bytes::<u32>(&[0x00, 0x00, 0x00, 0x01])?, 0x0100_0000);
 /// # */
-/// #   assert_eq!(from_bytes::<u32>(&[0x00, 0x00, 0x00, 0x01].le_to_native::<u32>()).unwrap(), 0x0100_0000);
+/// #   assert_eq!(from_bytes::<u32>(&Le2NAl4([0x00, 0x00, 0x00, 0x01]).0.le_to_native::<u32>()).unwrap(), 0x0100_0000);
 /// }
 /// # }
 /// ```
@@ -84,7 +84,7 @@ pub unsafe fn from_bytes<T: Copy>(bytes: &[u8]) -> Result<T, Error<u8, T>> {
 ///     assert_eq!(from_bytes_pedantic::<u32>(&[0x00, 0x00, 0x00, 0x01])?, 0x0100_0000);
 /// # */
 /// #   assert_eq!(
-/// #       from_bytes_pedantic::<u32>(&[0x00, 0x00, 0x00, 0x01].le_to_native::<u32>()).unwrap(),
+/// #       from_bytes_pedantic::<u32>(&Le2NAl4([0x00, 0x00, 0x00, 0x01]).0.le_to_native::<u32>()).unwrap(),
 /// #       0x0100_0000
 /// #   );
 /// }
@@ -129,7 +129,7 @@ pub unsafe fn from_bytes_pedantic<T: Copy>(bytes: &[u8]) -> Result<T, Error<u8, 
 ///     assert_eq!(
 ///         transmute_many::<u16, SingleManyGuard>(&[0x00, 0x01, 0x00, 0x02])?,
 /// # */
-/// #   assert_eq!(transmute_many::<u16, SingleManyGuard>(&[0x00, 0x01, 0x00, 0x02].le_to_native::<u16>()).unwrap(),
+/// #   assert_eq!(transmute_many::<u16, SingleManyGuard>(&Le2NAl4([0x00, 0x01, 0x00, 0x02]).0.le_to_native::<u16>()).unwrap(),
 ///         &[0x0100, 0x0200]
 ///     );
 /// }
@@ -174,7 +174,7 @@ pub unsafe fn transmute_many<T, G: Guard>(bytes: &[u8]) -> Result<&[T], Error<u8
 ///     assert_eq!(
 ///         transmute_many_mut::<u16, SingleManyGuard>(&mut [0xFF, 0x01, 0x00, 0x02])?,
 /// # */
-/// #   assert_eq!(transmute_many_mut::<u16, SingleManyGuard>(&mut [0xFF, 0x01, 0x00, 0x02].le_to_native::<u16>()).unwrap(),
+/// #   assert_eq!(transmute_many_mut::<u16, SingleManyGuard>(&mut Le2NAl4([0xFF, 0x01, 0x00, 0x02]).0.le_to_native::<u16>()).unwrap(),
 ///         &mut [0x01FF, 0x0200]
 ///     );
 /// }
@@ -215,7 +215,7 @@ pub unsafe fn transmute_many_mut<T, G: Guard>(bytes: &mut [u8]) -> Result<&mut [
 ///     assert_eq!(
 ///         transmute_many_permissive::<u16>(&[0x00, 0x01, 0x00, 0x02]),
 /// # */
-/// #   assert_eq!(transmute_many_permissive::<u16>(&[0x00, 0x01, 0x00, 0x02].le_to_native::<u16>()),
+/// #   assert_eq!(transmute_many_permissive::<u16>(&Le2NAl4([0x00, 0x01, 0x00, 0x02]).0.le_to_native::<u16>()),
 ///         &[0x0100, 0x0200]
 ///     );
 /// }

src/full.rs
@@ -189,7 +189,7 @@ pub fn transmute_many_pedantic<T: TriviallyTransmutable>(bytes: &[u8]) -> Result
 /// # /*
 /// assert_eq!(transmute_many_mut::<u16, SingleManyGuard>(&mut [0x00, 0x01, 0x00, 0x02])?,
 /// # */
-/// # assert_eq!(transmute_many_mut::<u16, SingleManyGuard>(&mut [0x00, 0x01, 0x00, 0x02].le_to_native::<u16>()).unwrap(),
+/// # assert_eq!(transmute_many_mut::<u16, SingleManyGuard>(&mut Le2NAl4([0x00, 0x01, 0x00, 0x02]).0.le_to_native::<u16>()).unwrap(),
 ///            &mut [0x0100, 0x0200]);
 /// # }
 /// ```
@@ -245,7 +245,7 @@ pub fn transmute_many_permissive_mut<T: TriviallyTransmutable>(bytes: &mut [u8])
 /// # /*
 /// assert_eq!(transmute_many_pedantic_mut::<u16>(&mut [0x0F, 0x0E, 0x0A, 0x0B])?,
 /// # */
-/// # assert_eq!(transmute_many_pedantic_mut::<u16>(&mut [0x0F, 0x0E, 0x0A, 0x0B].le_to_native::<u16>()).unwrap(),
+/// # assert_eq!(transmute_many_pedantic_mut::<u16>(&mut Le2NAl4([0x0F, 0x0E, 0x0A, 0x0B]).0.le_to_native::<u16>()).unwrap(),
 ///            &mut [0x0E0F, 0x0B0A]);
 /// # }
 /// ```

src/lib.rs
@@ -105,7 +105,7 @@
 //! # include!("../tests/test_util/le_to_native.rs");
 //! # #[cfg(feature = "std")]
 //! # fn main() -> Result<(), Box<std::error::Error>> {
-//! # let bytes = &[0x00, 0x01, 0x12, 0x34].le_to_native::<u16>();
+//! # let bytes = &Le2NAl4([0x00, 0x01, 0x12, 0x34]).0.le_to_native::<u16>();
 //! # let words = try_copy!(transmute_many_pedantic::<u16>(bytes).map_err(Error::without_src));
 //! # /*
 //! // Assuming little-endian