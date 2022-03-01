nabijaczleweli/safe-transmute-rs

commit 6e9520a6d9acdf5eac96b5dc8a15aa241cd2e0b7

Force-align remaining doctests
Ref: #67
Fixes: f3c892034d1624b99856c2a22c5ffa3b6bdd4e31 ("Fix tests that get
broken by rustc's new byte slice layout")

src/lib.rs
@@ -105,7 +105,7 @@
 //! # include!("../tests/test_util/le_to_native.rs");
 //! # #[cfg(feature = "std")]
 //! # fn main() -> Result<(), Box<std::error::Error>> {
-//! # let bytes = &[0x00, 0x01, 0x12, 0x34].le_to_native::<u16>();
+//! # let bytes = &Le2NAl4([0x00, 0x01, 0x12, 0x34]).0.le_to_native::<u16>();
 //! # let words = try_copy!(transmute_many_pedantic::<u16>(bytes).map_err(Error::without_src));
 //! # /*
 //! // Assuming little-endian

src/base.rs
@@ -44,7 +44,7 @@ use core::slice;
 /// # /*
 ///     assert_eq!(from_bytes::<u32>(&[0x00, 0x00, 0x00, 0x01])?, 0x0100_0000);
 /// # */
-/// #   assert_eq!(from_bytes::<u32>(&[0x00, 0x00, 0x00, 0x01].le_to_native::<u32>()).unwrap(), 0x0100_0000);
+/// #   assert_eq!(from_bytes::<u32>(&Le2NAl4([0x00, 0x00, 0x00, 0x01]).0.le_to_native::<u32>()).unwrap(), 0x0100_0000);
 /// }
 /// # }
 /// ```
@@ -84,7 +84,7 @@ pub unsafe fn from_bytes<T: Copy>(bytes: &[u8]) -> Result<T, Error<u8, T>> {
 ///     assert_eq!(from_bytes_pedantic::<u32>(&[0x00, 0x00, 0x00, 0x01])?, 0x0100_0000);
 /// # */
 /// #   assert_eq!(
-/// #       from_bytes_pedantic::<u32>(&[0x00, 0x00, 0x00, 0x01].le_to_native::<u32>()).unwrap(),
+/// #       from_bytes_pedantic::<u32>(&Le2NAl4([0x00, 0x00, 0x00, 0x01]).0.le_to_native::<u32>()).unwrap(),
 /// #       0x0100_0000
 /// #   );
 /// }
@@ -129,7 +129,7 @@ pub unsafe fn from_bytes_pedantic<T: Copy>(bytes: &[u8]) -> Result<T, Error<u8, 
 ///     assert_eq!(
 ///         transmute_many::<u16, SingleManyGuard>(&[0x00, 0x01, 0x00, 0x02])?,
 /// # */
-/// #   assert_eq!(transmute_many::<u16, SingleManyGuard>(&[0x00, 0x01, 0x00, 0x02].le_to_native::<u16>()).unwrap(),
+/// #   assert_eq!(transmute_many::<u16, SingleManyGuard>(&Le2NAl4([0x00, 0x01, 0x00, 0x02]).0.le_to_native::<u16>()).unwrap(),
 ///         &[0x0100, 0x0200]
 ///     );
 /// }
@@ -174,7 +174,7 @@ pub unsafe fn transmute_many<T, G: Guard>(bytes: &[u8]) -> Result<&[T], Error<u8
 ///     assert_eq!(
 ///         transmute_many_mut::<u16, SingleManyGuard>(&mut [0xFF, 0x01, 0x00, 0x02])?,
 /// # */
-/// #   assert_eq!(transmute_many_mut::<u16, SingleManyGuard>(&mut [0xFF, 0x01, 0x00, 0x02].le_to_native::<u16>()).unwrap(),
+/// #   assert_eq!(transmute_many_mut::<u16, SingleManyGuard>(&mut Le2NAl4([0xFF, 0x01, 0x00, 0x02]).0.le_to_native::<u16>()).unwrap(),
 ///         &mut [0x01FF, 0x0200]
 ///     );
 /// }
@@ -215,7 +215,7 @@ pub unsafe fn transmute_many_mut<T, G: Guard>(bytes: &mut [u8]) -> Result<&mut [
 ///     assert_eq!(
 ///         transmute_many_permissive::<u16>(&[0x00, 0x01, 0x00, 0x02]),
 /// # */
-/// #   assert_eq!(transmute_many_permissive::<u16>(&[0x00, 0x01, 0x00, 0x02].le_to_native::<u16>()),
+/// #   assert_eq!(transmute_many_permissive::<u16>(&Le2NAl4([0x00, 0x01, 0x00, 0x02]).0.le_to_native::<u16>()),
 ///         &[0x0100, 0x0200]
 ///     );
 /// }

src/full.rs
@@ -189,7 +189,7 @@ pub fn transmute_many_pedantic<T: TriviallyTransmutable>(bytes: &[u8]) -> Result
 /// # /*
 /// assert_eq!(transmute_many_mut::<u16, SingleManyGuard>(&mut [0x00, 0x01, 0x00, 0x02])?,
 /// # */
-/// # assert_eq!(transmute_many_mut::<u16, SingleManyGuard>(&mut [0x00, 0x01, 0x00, 0x02].le_to_native::<u16>()).unwrap(),
+/// # assert_eq!(transmute_many_mut::<u16, SingleManyGuard>(&mut Le2NAl4([0x00, 0x01, 0x00, 0x02]).0.le_to_native::<u16>()).unwrap(),
 ///            &mut [0x0100, 0x0200]);
 /// # }
 /// ```
@@ -245,7 +245,7 @@ pub fn transmute_many_permissive_mut<T: TriviallyTransmutable>(bytes: &mut [u8])
 /// # /*
 /// assert_eq!(transmute_many_pedantic_mut::<u16>(&mut [0x0F, 0x0E, 0x0A, 0x0B])?,
 /// # */
-/// # assert_eq!(transmute_many_pedantic_mut::<u16>(&mut [0x0F, 0x0E, 0x0A, 0x0B].le_to_native::<u16>()).unwrap(),
+/// # assert_eq!(transmute_many_pedantic_mut::<u16>(&mut Le2NAl4([0x0F, 0x0E, 0x0A, 0x0B]).0.le_to_native::<u16>()).unwrap(),
 ///            &mut [0x0E0F, 0x0B0A]);
 /// # }
 /// ```

commit 73a00ee37931303bc5c69480791e8720847c4f7f

Remove alignment-breaking LeToNative impls, fixing unsafe doctests
Ref: #67
Fixes: 6e9520a6d9acdf5eac96b5dc8a15aa241cd2e0b7 ("Force-align remaining
 doctests")

src/lib.rs
@@ -105,7 +105,7 @@
 //! # include!("../tests/test_util/le_to_native.rs");
 //! # #[cfg(feature = "std")]
 //! # fn main() -> Result<(), Box<std::error::Error>> {
-//! # let bytes = &Le2NAl4([0x00, 0x01, 0x12, 0x34]).0.le_to_native::<u16>();
+//! # let bytes = &Le2NAl4([0x00, 0x01, 0x12, 0x34]).le_to_native::<u16>();
 //! # let words = try_copy!(transmute_many_pedantic::<u16>(bytes).map_err(Error::without_src));
 //! # /*
 //! // Assuming little-endian
@@ -131,8 +131,8 @@
 //!     &[0x00, 0x00, 0x00, 0x00,
 //!       0x00, 0x00, 0x00, 0x40])?,
 //! # */
-//! #   &Le2NAl8([0x00, 0x00, 0x00, 0x00,
-//! #             0x00, 0x00, 0x00, 0x40]).0.le_to_native::<f64>()).unwrap(),
+//! #   &*Le2NAl8([0x00, 0x00, 0x00, 0x00,
+//! #              0x00, 0x00, 0x00, 0x40]).le_to_native::<f64>()).unwrap(),
 //!     2.0);
 //! # }
 //! ```
@@ -148,7 +148,7 @@
 //! # /*
 //!            &[0x01, 0x00, 0x34, 0x12]);
 //! # */
-//! #          &[0x01, 0x00, 0x34, 0x12].le_to_native::<u16>());
+//! #          &*Le2NAl4([0x01, 0x00, 0x34, 0x12]).le_to_native::<u16>());
 //! # }
 //! ```
 

src/to_bytes.rs
@@ -76,7 +76,7 @@ pub unsafe fn transmute_to_bytes_unchecked<S>(from: &S) -> &[u8] {
 /// # /*
 ///                &mut [0x67, 0x45, 0x23, 0x01]);
 /// # */
-/// #              &mut [0x67, 0x45, 0x23, 0x01].le_to_native::<u32>());
+/// #              (&mut [0x67, 0x45, 0x23, 0x01]).le_to_native::<u32>());
 /// }
 /// # }
 /// ```
@@ -286,7 +286,7 @@ pub fn transmute_one_to_bytes<S: TriviallyTransmutable>(from: &S) -> &[u8] {
 /// # /*
 ///            &mut [0x67, 0x45, 0x23, 0x01]);
 /// # */
-/// #          &mut [0x67, 0x45, 0x23, 0x01].le_to_native::<u32>());
+/// #          (&mut [0x67, 0x45, 0x23, 0x01]).le_to_native::<u32>());
 /// # }
 /// ```
 ///

src/trivial.rs
@@ -192,7 +192,7 @@ pub fn align_to_mut<S: TriviallyTransmutable, T: TriviallyTransmutable>(slice: &
 /// # /*
 ///     assert_eq!(transmute_trivial::<u32>(&[0x00, 0x00, 0x00, 0x01])?, 0x0100_0000);
 /// # */
-/// #   assert_eq!(transmute_trivial::<u32>(&[0x00, 0x00, 0x00, 0x01].le_to_native::<u32>()).unwrap(), 0x0100_0000);
+/// #   assert_eq!(transmute_trivial::<u32>(&Le2NAl4([0x00, 0x00, 0x00, 0x01]).le_to_native::<u32>()).unwrap(), 0x0100_0000);
 /// }
 /// # }
 /// ```
@@ -230,7 +230,7 @@ pub unsafe fn transmute_trivial<T: TriviallyTransmutable>(bytes: &[u8]) -> Resul
 /// # /*
 ///     assert_eq!(transmute_trivial_pedantic::<u16>(&[0x0F, 0x0E])?, 0x0E0F);
 /// # */
-/// #   assert_eq!(transmute_trivial_pedantic::<u16>(&[0x0F, 0x0E].le_to_native::<u16>()).unwrap(), 0x0E0F);
+/// #   assert_eq!(transmute_trivial_pedantic::<u16>(&Le2NAl2([0x0F, 0x0E]).le_to_native::<u16>()).unwrap(), 0x0E0F);
 /// }
 /// # }
 /// ```
@@ -266,7 +266,7 @@ pub unsafe fn transmute_trivial_pedantic<T: TriviallyTransmutable>(bytes: &[u8])
 /// # /*
 ///     assert_eq!(transmute_trivial_many::<u16, SingleManyGuard>(&[0x00, 0x01, 0x00, 0x02])?,
 /// # */
-/// #   assert_eq!(transmute_trivial_many::<u16, SingleManyGuard>(&[0x00, 0x01, 0x00, 0x02].le_to_native::<u16>()).unwrap(),
+/// #   assert_eq!(transmute_trivial_many::<u16, SingleManyGuard>(&Le2NAl4([0x00, 0x01, 0x00, 0x02]).le_to_native::<u16>()).unwrap(),
 ///                &[0x0100, 0x0200]);
 /// }
 /// # }
@@ -303,7 +303,7 @@ pub unsafe fn transmute_trivial_many<T: TriviallyTransmutable, G: Guard>(bytes: 
 /// # /*
 ///     assert_eq!(transmute_trivial_many::<u16, SingleManyGuard>(&[0x00, 0x01, 0x00, 0x02])?,
 /// # */
-/// #   assert_eq!(transmute_trivial_many::<u16, SingleManyGuard>(&[0x00, 0x01, 0x00, 0x02].le_to_native::<u16>()).unwrap(),
+/// #   assert_eq!(transmute_trivial_many::<u16, SingleManyGuard>(&Le2NAl4([0x00, 0x01, 0x00, 0x02]).le_to_native::<u16>()).unwrap(),
 ///                &[0x0100, 0x0200]);
 /// }
 /// # }

tests/test_util/le_to_native.rs
@@ -1,18 +1,6 @@
 extern crate core as le_to_native_core;
 
 
-/// Test aids: rustc has started placing static byte arrays at odd offsets
-#[repr(align(64))]
-#[allow(dead_code)]
-struct Le2NAl2([u8; 2]);
-#[repr(align(64))]
-#[allow(dead_code)]
-struct Le2NAl4([u8; 4]);
-#[repr(align(64))]
-#[allow(dead_code)]
-struct Le2NAl8([u8; 8]);
-
-
 /// Verify: http://play.integer32.com/?gist=4cd795d6f45898c876a754cd3f3c2aaa&version=stable
 trait LeToNative {
     fn le_to_native<T: Sized>(self) -> Self;
@@ -49,9 +37,40 @@ impl LeToNative for Vec<u8> {
     }
 }
 
-macro_rules! impl_le_to_native_array_u8 {
-    ($n:expr) => {
-        impl LeToNative for [u8; $n] {
+/// Test aids: rustc has started placing static byte arrays at odd offsets
+macro_rules! impl_le_to_native_Le2NAl {
+    ($nm:ident, $n:expr) => {
+        #[repr(align(64))]
+        #[allow(dead_code)]
+        struct $nm([u8; $n]);
+
+        impl le_to_native_core::borrow::Borrow<[u8]> for $nm {
+            fn borrow(&self) -> &[u8] {
+                &self.0
+            }
+        }
+
+        impl le_to_native_core::borrow::BorrowMut<[u8]> for $nm {
+            fn borrow_mut(&mut self) -> &mut [u8] {
+                &mut self.0
+            }
+        }
+
+        impl le_to_native_core::ops::Deref for $nm {
+            type Target = [u8];
+
+            fn deref(&self) -> &[u8] {
+                &self.0
+            }
+        }
+
+        impl le_to_native_core::ops::DerefMut for $nm {
+            fn deref_mut(&mut self) -> &mut [u8] {
+                &mut self.0
+            }
+        }
+
+        impl LeToNative for $nm {
             #[cfg(target_endian = "little")]
             fn le_to_native<T: Sized>(self) -> Self {
                 self
@@ -59,42 +78,13 @@ macro_rules! impl_le_to_native_array_u8 {
 
             #[cfg(target_endian = "big")]
             fn le_to_native<T: Sized>(mut self) -> Self {
-                (&mut self[..]).le_to_native::<T>();
+                (&mut self.0[..]).le_to_native::<T>();
                 self
             }
         }
     }
 }
 
-impl_le_to_native_array_u8!(1);
-impl_le_to_native_array_u8!(2);
-impl_le_to_native_array_u8!(3);
-impl_le_to_native_array_u8!(4);
-impl_le_to_native_array_u8!(5);
-impl_le_to_native_array_u8!(6);
-impl_le_to_native_array_u8!(7);
-impl_le_to_native_array_u8!(8);
-impl_le_to_native_array_u8!(9);
-impl_le_to_native_array_u8!(10);
-impl_le_to_native_array_u8!(11);
-impl_le_to_native_array_u8!(12);
-impl_le_to_native_array_u8!(13);
-impl_le_to_native_array_u8!(14);
-impl_le_to_native_array_u8!(15);
-impl_le_to_native_array_u8!(16);
-impl_le_to_native_array_u8!(17);
-impl_le_to_native_array_u8!(18);
-impl_le_to_native_array_u8!(19);
-impl_le_to_native_array_u8!(20);
-impl_le_to_native_array_u8!(21);
-impl_le_to_native_array_u8!(22);
-impl_le_to_native_array_u8!(23);
-impl_le_to_native_array_u8!(24);
-impl_le_to_native_array_u8!(25);
-impl_le_to_native_array_u8!(26);
-impl_le_to_native_array_u8!(27);
-impl_le_to_native_array_u8!(28);
-impl_le_to_native_array_u8!(29);
-impl_le_to_native_array_u8!(30);
-impl_le_to_native_array_u8!(31);
-impl_le_to_native_array_u8!(32);
+impl_le_to_native_Le2NAl!(Le2NAl2, 2);
+impl_le_to_native_Le2NAl!(Le2NAl4, 4);
+impl_le_to_native_Le2NAl!(Le2NAl8, 8);